7054049bec475fc6b69986f71ab9c407ae201b42
Sovan <80006154+JaysonSovanHong@users.noreply.github.com>
2021-03-20 16:38:13 -0400

    Add files via upload

---
+++ code.js
@@ -0,0 +1,23 @@
+// const keepScore = document.querySelector('.keepScore');
+// // const button = document.querySelector('.button');
+
+// const container = document.querySelector('.container');
+// const box = document.querySelectorAll('.box');
+
+const newGame = document.querySelector('.newGame');
+
+document.querySelector('.newGame').addEventListener("click,", function() => {
+
+        }
+
+
+        // box.forEach(card => card.addEventListener('click', ))
+
+        // function refreshPage() {
+        //     if (confirm(`New Game`)) {
+        //         url.refreshPage();
+        //     }
+
+
+
+        // }
